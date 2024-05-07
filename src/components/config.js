b82f2b3a0c5019eaaba1129134ca322e86d160b0
hectordiazgomez <konlaptechs@gmail.com>
2024-05-06 21:15:33 -0500

    first commit

---
+++ src/components/config.js
@@ -0,0 +1,11 @@
+export const avatarAppConfig = {
+    cogSvcRegion: "Global",
+    cogSvcSubKey: "HulPbIhH9Q4R67Xu3kKP950faup+rbfD3LG8K2RcZAbKDEesFDQCVCN776ZfMRTrDXxiRviMbJkoz9CDdQ3rTg==",
+    voiceName: "en-US-JennyNeural",
+    avatarCharacter: "lisa",
+    avatarStyle: "casual-sitting",
+    avatarBackgroundColor: "#FFFFFFFF",
+    iceUrl: "stun:relay.communication.microsoft.com:3478",
+    iceUsername: "YOUR USERNAME",
+    iceCredential: "YOUR CREDENTIAL"
+}
